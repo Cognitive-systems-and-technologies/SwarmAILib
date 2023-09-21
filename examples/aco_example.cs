3a3864bed80a3885e9b5923e33ff9ef8bfab13be
CognitiveTechnologies <111611001+CognitiveTechnologies@users.noreply.github.com>
2023-09-22 00:57:56 +0300

    Update aco_example.cs

--- examples/aco_example.cs
+++ examples/aco_example.cs
@@ -1,1 +1,24 @@
-Console.WriteLine("Hello, world!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем новый лабиринт
+        Maze maze = new Maze(10, 10);
+
+        // Создаем новый экземпляр алгоритма колонии муравьев
+        AntColonyAlgorithm antColonyAlgorithm = new AntColonyAlgorithm(maze, 100, 1000, 1.0, 1.0, 0.5, 1.0);
+
+        // Запускаем алгоритм
+        antColonyAlgorithm.Run();
+
+        // Выводим результат алгоритма
+        for (int i = 0; i < maze.GetRows(); i++)
+        {
+            for (int j = 0; j < maze.GetColumns(); j++)
+            {
+                Console.Write(antColonyAlgorithm.GetPheromoneLevel(i, j) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
